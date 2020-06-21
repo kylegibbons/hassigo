0bc44f16b390f99749449ff7b367c86f2a704171
Kyle Gibbons <kyle@kylegibbons.com>
2020-06-21 11:42:00 -0400

    Build test

--- test.ps1
+++ test.ps1
@@ -3,4 +3,4 @@
 
 #docker run --rm --privileged -v ~/.docker:/root/.docker -v addon:/data:ro homeassistant/amd64-builder --all -t /data
 
-docker run --rm --privileged -v ~/.docker:/root/.docker homeassistant/amd64-builder --all -t addon -r https://github.com/kylegibbons/hassigo -b master
+docker run --rm --privileged -v ~/.docker:/root/.docker homeassistant/amd64-builder --all -t addon -r https://github.com/kylegibbons/hassigo -b master --generic 1
