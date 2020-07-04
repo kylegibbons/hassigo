a9df569cea131222da5eed70b554ce6f5bbb9de3
Kyle Gibbons <kyle@kylegibbons.com>
2020-07-04 10:55:26 -0400

    Corrected file name

--- test.ps1
+++ test.ps1
@@ -5,7 +5,7 @@
 
 #docker run --rm --privileged -v ~/.docker:/root/.docker homeassistant/amd64-builder --all -t addon -r https://github.com/kylegibbons/hassigo -b master --generic 1
 
-docker run -it --rm --privileged -v ~/.docker:/root/.docker -v "$(pwd)/addon:/addon" homeassistant/amd64-builder --amd64 -t /addon
+docker run -it --rm --privileged -v ~/.docker:~/.docker -v "$(pwd)/addon:/addon" homeassistant/amd64-builder --amd64 -t /addon
 
 
 #docker run -it --privileged -v ./addon/:/addon ubuntu bash
