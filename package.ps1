a593183cdae48b8eb6364fa80a688690af3ced6f
Kyle Gibbons <kyle@kylegibbons.com>
2020-07-04 10:47:01 -0400

    Trying to push

--- package.ps1
+++ package.ps1
@@ -3,8 +3,8 @@ remove-item -Recurse -Path ./addon/bin | Out-Null
 $env:CGO_ENABLED = 0 
 $env:GOOS = "linux"
 $env:GOARCH = "amd64"
-go build -a -o ./addon/bin/hassigo
+go build -a -o ./addon/bin/hassigo ./cmd/hassigo
 
-copy-item .\raw.html ./addon/bin | Out-Null
+copy-item ./cmd/hassigo/raw.html ./addon/bin | Out-Null
 
-docker build -t kylegibbons/hassigo --build-arg BUILD_FROM=hassioaddons/base-amd64:7.2.0 ./addon 
+#docker build -t kylegibbons/hassigo --build-arg BUILD_FROM=hassioaddons/base-amd64:7.2.0 ./addon 
--- test.ps1
+++ test.ps1
@@ -4,7 +4,8 @@
 #docker run --rm --privileged -v ~/.docker:/root/.docker -v addon:/data:ro homeassistant/amd64-builder --all -t /data
 
 #docker run --rm --privileged -v ~/.docker:/root/.docker homeassistant/amd64-builder --all -t addon -r https://github.com/kylegibbons/hassigo -b master --generic 1
-#docker run -it --rm --privileged -v ~/.docker:/root/.docker -v ./addon:/addon homeassistant/amd64-builder --amd64 -t /addon
+
+docker run -it --rm --privileged -v ~/.docker:/root/.docker -v "$(pwd)/addon:/addon" homeassistant/amd64-builder --amd64 -t /addon
 
 
-docker run -it --privileged -v ./addon/:/addon ubuntu bash
+#docker run -it --privileged -v ./addon/:/addon ubuntu bash
